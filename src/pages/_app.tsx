2d8e837f6cdaa7dffe699686da726d546b3b255a
Afsoon <said.atrahouch@gmail.com>
2020-04-18 16:28:08 +0200

    seo changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,39 @@ class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>Pelatencasa</title>
+          <title>SOS Pelatencasa</title>
+          <meta
+            name="description"
+            content="Servicio online de asistencia a la peluquería con tu peluquero de confianza."
+          />
+          <meta name="robots" content="index, follow" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="SOS Pelatencasa" />
+          <meta property="og:url" content="https://pelatencasa.now.sh/" />
+          <meta
+            property="og:image"
+            content="https://pelatencasa.now.sh/seo-image.png"
+          />
+          <meta property="og:image:width" content="484" />
+          <meta property="og:image:height" content="500" />
+          <meta
+            property="og:description"
+            content="Servicio online de asistencia a la peluquería con tu peluquero de confianza."
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:url" content="https://pelatencasa.now.sh/" />
+          <meta name="twitter:title" content="SOS Pelatencasa" />
+          <meta
+            name="twitter:description"
+            content="Servicio online de asistencia a la peluquería con tu peluquero de confianza."
+          />
+          <meta
+            name="twitter:image"
+            content="https://pelatencasa.now.sh/seo-image.png"
+          />
 
           <link
             rel="apple-touch-icon"
@@ -42,11 +74,7 @@ class MyApp extends App {
           />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-          <meta name="msapplication-TileColor" content="#00aba9" />
-          <meta name="theme-color" content="#ffffff" />
-
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
