466949e20df42209abadf2d8575a03bd94f7f538
Afsoon <said.atrahouch@gmail.com>
2020-04-18 15:08:20 +0200

    icons for multiples screens, devices and SO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,41 @@ class MyApp extends App {
       <React.Fragment>
         <Head>
           <title>Pelatencasa</title>
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="194x194"
+            href="/favicon-194x194.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
+
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
