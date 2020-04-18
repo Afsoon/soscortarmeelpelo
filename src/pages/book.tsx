d074091b95978bec5be46021f04a89f0a6c089dd
Afsoon <said.atrahouch@gmail.com>
2020-04-18 12:45:51 +0200

    remove acent

--- src/pages/book.tsx
+++ src/pages/book.tsx
@@ -8,7 +8,7 @@ const CTA = () => (
       <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
         ¿Preparado?
         <br />
-        Pide tú cita hoy.
+        Pide tu cita hoy.
       </h2>
       <div className="mt-8 flex justify-center">
         <iframe
