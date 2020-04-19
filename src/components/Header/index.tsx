715229b849651d14f058192bba611eda58f03dda
Afsoon <said.atrahouch@gmail.com>
2020-04-19 07:56:11 +0200

    fix url in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -65,7 +65,7 @@ const Header: React.FunctionComponent<HeaderProps> = ({
             )}
             {showContact && (
               <a
-                href="#"
+                href="/contact"
                 title="Enviar mensaje de contacto"
                 className="ml-10 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out"
               >
