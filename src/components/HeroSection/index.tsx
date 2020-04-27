980526f6569c8ac56c3e887351a77a544f901d1a
Afsoon <said.atrahouch@gmail.com>
2020-04-27 17:21:46 +0200

    Added iframe video

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -235,35 +235,14 @@ const HeroSection = () => {
                 />
               </svg>
               <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-                <button
-                  aria-label="Not complain to remove"
-                  className="relative block w-full rounded-lg overflow-hidden focus:outline-none focus:shadow-outline"
-                >
-                  <picture>
-                    <source srcSet="/placeholder.webp" type="image/webp" />
-                    <img
-                      className="w-full"
-                      src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-                      alt="Dependienta haciendo una venta"
-                    />
-                  </picture>
-                  <div className="absolute inset-0 w-full h-full flex items-center justify-center">
-                    <svg
-                      className="h-20 w-20 text-indigo-500"
-                      fill="currentColor"
-                      viewBox="0 0 84 84"
-                    >
-                      <circle
-                        opacity="0.9"
-                        cx="42"
-                        cy="42"
-                        r="42"
-                        fill="white"
-                      />
-                      <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
-                    </svg>
-                  </div>
-                </button>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube-nocookie.com/embed/bS6T4U-IwIQ"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
               </div>
             </div>
           </div>
